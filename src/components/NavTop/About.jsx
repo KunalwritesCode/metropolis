1e51ab706dd965a6ded8ad1ad09222bb19864243
Kunal <kunalpidiha57@gmail.com>
2023-01-02 15:22:36 +0530

    About Us Completed

--- src/components/NavTop/About.jsx
+++ src/components/NavTop/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
          <div>
          <img src='https://www.metropolisindia.com/newdata/images/group-169.svg' />
           <h6>125+</h6>
-          <p>Laboratories</p>
+          <p>Our Laboratories</p>
          </div>
          </div>
          
@@ -53,14 +53,63 @@ const About = () => {
          <div>
          <img src='https://www.metropolisindia.com/newdata/images/group-169.svg' />
           <h6>125+</h6>
-          <p>Laboratories</p>
+          <p>Our Laboratories</p>
          </div>
          </div>
 
-         <div className=''>
-
+         <div className='About-container-Aboutus-top-2-para'>
+          <p>The Company’s broad spectrum of services offer around 3,487 clinical laboratory <br />tests and 530 profiles. Various test combinations, specific to a disease or disorder<br /> and wellness profiles used for health and fitness screening form a part of the Company’s<br /> service profile.</p>
+          <p>Metropolis has invested in a variety of specialized tests and also adopted several  <br />advanced tests and technologies introduced in the global market. As on date, the Company offers 2,799 specialized tests. One of the prime reasons behind the Company’s leading position in the diagnostics industry is its track record of introducing specialized tests.</p>
          </div>
          
+       </div>
+
+       <div className='About-container-Aboutus-middle'>
+          <div className='About-container-Aboutus-middle-first'>
+            <h2>Chairman's Message</h2>
+            <h6>Putting our patients first: Delivering reliable pathology reports</h6>
+            <p>It gives me immense satisfaction to pen down this message as the Chairman of Metropolis Healthcare Ltd. It has been a long and humbling journey for me personally to see Metropolis grow as a reputed and trusted pathology brand all across India and markets in Africa. I started out with a single minded focus on putting our patients’ first and a relentless pursuit in delivering accurate results and quality reports.</p>
+            <p>Our promise to the patient is very simple; reliable pathology reports and for that we walk the extra mile for recheck and reflex at no additional cost to our patients.</p>
+            <h6>Working in tandem with doctors because pathology matters</h6>
+            <p>More than 70% of medical decisions today are based on diagnostic reports. My team of doctors is constantly in touch with clinicians, work in tandem and help them diagnose and make treatment decisions for their patients. Be it a simple blood sugar test or a complicated molecular genetic test, we have built in all the capabilities to deliver the absolute best.</p>
+            <h6>Quality: A way of functioning at Metropolis</h6>
+            <p>Metropolis has a dedicated quality assurance team that works on accreditations for our various centers. Our Global Reference Laboratory in Mumbai is accredited by CAP (College of American Pathologists), a reputed global accreditation board and NABL (National Accreditation Board for Testing & Calibration Laboratories).To give a perspective to the benefit of our shareholders, our CAP Proficiency Testing Scores are above 98% indicating consistently good proficiency.</p>
+            <p>Metropolis is the only private laboratory to have in-house developed and NABL accredited EQAS program. This program is used to monitor analytical quality of our group labs and 115 group labs is covered currently and is ready for commercial launch for customers.</p>
+            <span>Warm regards,</span>
+            <h6>Dr Sushil Shah</h6>
+            <p>Founder & Chairman</p>
+          </div>
+          <div className='About-container-Aboutus-middle-second'>
+            <img src='https://www.metropolisindia.com/newdata/images/drsunilshah.jpg' />
+            <h2>Dr Sushil Shah</h2>
+            <p>Chairman and Executive Director</p>
+            <span>"Metropolis has always been<br /> at the forefront in bringing new<br /> tests and technologies to the people.<br /> our test menu comprised of 3,487 clinical laboratory tests and over 530 test profiles."</span>
+          </div>
+       </div>
+       <div className='About-container-Aboutus-bottom'>
+         <div className='About-container-Aboutus-bottom-first'>
+          <img src='https://www.metropolisindia.com/newdata/images/lady.png' />
+          <h2>Ms Ameera Shah</h2>
+          <p>Managing Director</p>
+          <span>
+          "As one of the country’s leading diagnostic companies, MHL continues building and nurturing <br />its talent. Several employee training and development initiatives are often undertaken. This helps incorporate integrity as a core ingredient of the Company’s work culture.""
+          </span>
+         </div>
+         <div className='About-container-Aboutus-bottom-second'>
+          <h2>MD’s Message</h2>
+          <p>To begin with, I would like to bring your attention to an Indicator that I have been passionate and relentless about. I am happy to share with you that in FY19, our Net Promoter Score has been on average at 85, which means that 85% of our customers are willing to recommend Metropolis to their family and network. Our Chairman in his note has emphasized on Metropolis’ core vision of putting the patients first and our customer loyalty score is an outcome of this vision.</p>
+          <p>Moving on to our performance review, we continue to maintain our position in the leaders’ quadrant and are ranked amongst the Top 2 companies in the fast growing Indian diagnostic industry.</p>
+          <h6>Going closer to the patient</h6>
+          <p>FY19 has ended on a positive note for us as the company delivered faster than industry growth. We are positive on the short and medium term growth for Metropolis owing to fast paced network expansion, relentless execution, strong brand and customer first philosophy. We are happy to report a 15.5% growth in number of patient visits with increase in revenue per patient (2.1%) and revenue per test (11.2%), which are better than current industry standards.</p>
+          <h6>Bolstering our growth in the wellness space</h6>
+          <p>Our wellness initiative ‘TruHealth’ was launched across 36 cities and we are confident that this will help us bolster our position in the fast growing wellness segment. It is heartening to see a positive trend of customers choosing to safeguard their health by undergoing preventive screening and diagnose conditions before they turn in to complications.</p>
+          <h6>The Metropolis Family</h6>
+          <p>Our people are at the core of everything we do. Our highly experienced leadership team leads day to day business and operations. Metropolis’ strong scientific and technical team of over 2000 members is led by a team of 200 doctors and pathologists. Our phlebotomists and home service technicians are best in class, highly skilled and trained to collect samples in a single prick and are efficient in drawing samples from infants, small children and the elderly. We will make continuous efforts to progress on diversity and inclusion as we move forward.</p>
+          <span>Yours truly,</span>
+          <h6>Ameera Shah</h6>
+          <p>Managing Director</p>
+         </div>
+
        </div>
        </div>
     </div>
